806bc3301a45b0c0e0636ee8c61c3d4422a3b0af
Kartik Gautam <95581576+kartikgautamz@users.noreply.github.com>
2024-09-10 10:52:17 +0530

    Add files via upload

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Kartik Gautam on 31/03/2024.
+//
+
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        
+        playSound(soundName: sender.currentTitle!)
+   
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
